db7642d12e284df2ac90ce4a44f618147ee0a46b
segun <segundo.marcaida@ing.austral.edu.ar>
2023-05-14 16:05:42 -0300

    initial commit

---
+++ src/test/kotlin/com/formatter/FormatterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.formatter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FormatterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
